e36274c7d0975ada3f8e8487466f7a7b2a8ff810
Josh Whiteside <jwhiteside2015@outlook.com>
2021-02-08 11:11:47 -0500

    Add elements to Main Window

--- HexToBase64Converter/HexToBase64Converter/MainWindow.xaml.cs
+++ HexToBase64Converter/HexToBase64Converter/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace HexToBase64Converter
 {
@@ -23,6 +12,16 @@ namespace HexToBase64Converter
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ConvertHexToBase64(HexString.Text);
+        }
+
+        private void ConvertHexToBase64(string hexString)
+        {
+            
         }
     }
 }
